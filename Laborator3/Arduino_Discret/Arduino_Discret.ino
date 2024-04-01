46046c7ca6feacb21d8ec2c2158cc884f8360b54
Recea Alex <recea.alex@yahoo.com>
2024-04-01 15:14:08 +0300

    adaugat fisier pentru lab 3 si creat arduino sketch, cod functional

--- Laborator3/Arduino_Discret/Arduino_Discret.ino
+++ Laborator3/Arduino_Discret/Arduino_Discret.ino
@@ -1,9 +1,51 @@
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+float c[3]={0,0,0}; 
+float e[3]={0,0,0}; 
+float in() 
+{ 
+union u_tag 
+{ 
+byte b[4]; 
+float fvalue; 
+} in; 
+in.fvalue=0; 
+for (int i=0 ; i<4 ; i++) 
+{ 
+while (!Serial.available()); 
+in.b[i]=Serial.read(); 
+} 
+return in.fvalue; 
+} 
+void out(float c) 
+{ 
+union u_tag 
+{ 
+byte b[4]; 
+float fvalue; 
+}out; 
+out.fvalue=c; 
+Serial.write(out.b[0]); 
+Serial.write(out.b[1]); 
+Serial.write(out.b[2]); 
+Serial.write(out.b[3]); 
+} 
+void setup() 
+{ 
+Serial.begin(9600); 
+delay(1000); 
+out(0); 
+} 
+void loop() 
+{ 
+e[0]=in(); 
+//insert below the calculated discrete control function 
+double b0 = 1.0046;
+double b1 = -0.9961;
+c[0] = c[1] + b0*e[0] + b1*e[1];
+c[0]= 1; 
+out(c[0]); 
+for(int i=2;i>0;i--) 
+{ 
+c[i]=c[i-1]; 
+e[i]=e[i-1]; 
+} 
 }
